96c05f6a3af6ae63fb98a884b48f590338cf0921
n <rodrigoword2@gmail.com>
2024-07-11 17:00:42 -0400

    new

---
+++ Atividade_02_apostila/questao01.py
@@ -0,0 +1,9 @@
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+
+
+maior = max(valor_01,valor_02)
+menor = min(valor_01, valor_02)
+
+print(f"O maior valor é: {maior}")
+print(f"O menor valor é: {menor}")
