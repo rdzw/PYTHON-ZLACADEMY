8a3d3f94ac6682c0cab9750134f9a832f53c408b
lima <2015002730@ifam.edu.br>
2024-07-16 02:59:23 -0400

    add novas_questoes

---
+++ exercicios_beecrowd/1007.py
@@ -0,0 +1,11 @@
+# Leia quatro valores inteiros A, B, C e D.
+
+A = int(input("Digite um valor: "))
+B = int(input("Digite um valor: "))
+C = int(input("Digite um valor: "))
+D = int(input("Digite um valor: "))
+
+# alcule e mostre a diferença do produto de A e B pelo produto de C e D 
+DIFERENCA = (A * B - C * D)
+
+print(f"DIFERENCA = {DIFERENCA}")
